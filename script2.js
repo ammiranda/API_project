06957ada60dc3b5393a41ed32a17e6fd7cb7987c
Alexander <alexandermichaelmiranda@gmail.com>
2013-07-22 08:02:12 -0400

    New updates

--- script2.js
+++ script2.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
-$.ajax("http://search.ams.usda.gov/FarmersMarkets/v1/data.svc/zipSearch?zip=22203")
-  .done(function(data){
-    console.log(data);
-  });
-
-$.ajax("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=1004457")
-  .done(function(data){
-    console.log(data);
-  });
-});
+	function getPrimes(max) { //Function to find a set of prime numbers with the max number of the set as the parameter
+    var sieve = [], i, j, primes = [];
+    for (i = 2; i <= max; ++i) {
+        if (!sieve[i]) {
+            // i has not been marked -- it is prime
+            primes.push(i);
+            for (j = i << 1; j <= max; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    return primes;
+}
+ 	var prime_list = getPrimes(1240);
+ 	console.log(prime_list);
+ 	var moo = $.getJSON("http://xkcd.com/info.0.json/callback=?");
+ 	console.log(moo);
+ 	
+ 	/*$.ajax({
+ 		url: "http://xkcd.com/info." + prime_list + ".json/",
+ 		dataType: 'jsonp',
+ 		success: function(*/
+ 	});
