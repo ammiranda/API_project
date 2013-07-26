3470e5d3c329e6e6cb51bff6b1b79f281e26b756
Alexander <alexandermichaelmiranda@gmail.com>
2013-07-26 18:24:25 -0400

    Ajax updates

--- script2.js
+++ script2.js
@@ -17,9 +17,26 @@ $(document).ready(function() {
  	//var moo = $.getJSON("http://xkcd.com/info.0.json/callback=?");
  	//console.log(moo);
  	
+ 	$('#random_Num').click(function(event){
+ 	event.preventDefault();
+ 	$('#comic_num').remove();
+ 	var i = Math.floor(Math.random() * 1240);
+ 	console.log(i);
  	$.ajax({
- 		url: "http://dynamic.xkcd.com/api-0/jsonp/comic/" + prime_list[10],
+ 		url: "http://dynamic.xkcd.com/api-0/jsonp/comic/" + prime_list[i],
  		dataType: 'jsonp',
+ 		beforeSend: function() {
+ 			$('#comic_view').append('<p id="loading">Loading Comic! Please Standby.</p>');
+ 			},
+ 		complete: function() {
+ 			$('#loading').remove();
+ 			},
  		success: function(data, textStatus, jqXHR) {
  			console.log(data);
- 	}})});
+ 			$('#comic_view').attr('src', data.img);
+ 			$('#header').append('<p id="comic_num">Comic Number: ' + prime_list[i] + '</p>');
+ 	}
+ 		//error: function(data, textStatus, jqXHR) {
+ 			
+ 		//	}
+ 	})})});
