aed87efc921dad44d5dfa825f8473f5fc91f8abe
Alexander <alexandermichaelmiranda@gmail.com>
2013-07-27 08:58:15 -0400

    CSS changes

--- script2.js
+++ script2.js
@@ -15,16 +15,28 @@ $(document).ready(function() {
  	//var moo = $.getJSON("http://xkcd.com/info.0.json/callback=?");
  	//console.log(moo);
  	
+ 	function getMax() {
+ 		$.ajax({
+ 			url: "http://dynamic.xkcd.com/api-0/jsonp/comic/",
+ 			dataType: 'jsonp',
+ 			success: function(data, textStatus, jqXHR) {
+ 				$('#comic_view').attr('src', data.img);
+ 				$('#header').append('<p id="comic_num">Comic Number: <br><span id="prim_num">' + data.num + '</span></p>');
+ 				}
+ 				})};
+ 	getMax();
+ 	
  	$('#random_Num').click(function(event){
  	event.preventDefault();
- 	var prime_list = getPrimes(1240);
+ 	var prime_list = getPrimes(1244);
+ 	var i = Math.floor(Math.random() * prime_list.length);
  	$('#comic_num').remove();
  	$('#comic_view').attr('src', ''); 	
- 	var i = Math.floor(Math.random() * 1241);
  	console.log(i);
  	$.ajax({
  		url: "http://dynamic.xkcd.com/api-0/jsonp/comic/" + prime_list[i],
  		dataType: 'jsonp',
+ 		cache: false,
  		beforeSend: function() {
  			$('#comic_view').hide();
  			$('#viewer').append('<p id="loading">Loading Comic! Please Standby.</p>');
@@ -36,7 +48,7 @@ $(document).ready(function() {
  			console.log(data);
  			$('#comic_view').attr('src', data.img);
  			$('#comic_view').show();
- 			$('#header').append('<p id="comic_num">Comic Number: ' + prime_list[i] + '</p>');
+ 			$('#header').append('<p id="comic_num">Comic Number: <br><span id="prim_num">' + prime_list[i] + '</span></p>');
  	}
  		//error: function(data, textStatus, jqXHR) {
  			
