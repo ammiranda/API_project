e67ddfa4894bbfe2099aad70d0cf0bc6e1d1e702
Alexander <alexandermichaelmiranda@gmail.com>
2014-01-13 11:19:36 -0800

    Modifications to script.js file

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
- 	//var moo = $.getJSON("http://xkcd.com/info.0.json/callback=?");
- 	//console.log(moo);
- 	
- 	//var prime_list2 = getPrimes(1247);
- 	//console.log(data);
  	function getMax(callback) {
         var returned_data = 0;
  		$.ajax({
-            async: false,
  			url: "http://dynamic.xkcd.com/api-0/jsonp/comic/",
  			dataType: 'jsonp',
  			success: function(data, textStatus, jqXHR) {
@@ -15,34 +9,31 @@
  				$('#comic_view').attr('src', data.img);
  				$('#header').append('<p id="comic_num">Comic Number: <br><span id="prim_num">' + data.num + '</span></p>');
  				console.log(prime_list2);
-                callback(returned_data);
+                callback(prime_list2);
  				}
  				});
     };
 
-    console.log("maxi returned: " + getMax(function(m) {
-        return m;
-    }));
  	
  	function getPrimes(max) { //Function to find a set of prime numbers with the max number of the set as the parameter
-    var sieve = [], i, j, primes = [];
-    for (i = 2; i <= max; ++i) {
-        if (!sieve[i]) {
-            // i has not been marked -- it is prime
-            primes.push(i);
-            for (j = i << 1; j <= max; j += i) {
-                sieve[j] = true;
+        var sieve = [], i, j, primes = [];
+        for (i = 2; i <= max; ++i) {
+            if (!sieve[i]) {
+                // i has not been marked -- it is prime
+                primes.push(i);
+                for (j = i << 1; j <= max; j += i) {
+                    sieve[j] = true;
+                }
             }
         }
-    }
-    return primes;
-    }
+        return primes;
+    };
 
 $(document).ready(function() {
  	
  	$('#random_Num').click(function(event){
      	event.preventDefault();
-     	var prime_list = getPrimes(1400);
+     	var prime_list = getMax(function(m) {prime_list = m;})();
      	var i = Math.floor(Math.random() * prime_list.length);
      	var te;
      	console.log(i);
