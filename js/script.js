d13c4787256563ac22d9a0d19e7ed2a034672a53
Alexander <alexandermichaelmiranda@gmail.com>
2014-01-13 15:41:09 -0800

    Added comments to script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-    function getPrimes(max) { //Function to find a set of prime numbers with the max number of the set as the parameter
-        var sieve = [], i, j, primes = [];
+    function getPrimes(max) {                  //Function to find a set of prime numbers with the max number of the set as the parameter
+        var sieve = [], i, j, primes = [];     
         for (i = 2; i <= max; ++i) {
             if (!sieve[i]) {
-                // i has not been marked -- it is prime
                 primes.push(i);
                 for (j = i << 1; j <= max; j += i) {
                     sieve[j] = true;
@@ -13,15 +12,15 @@
     };
 
     function loadFirstComic(data) {
-        var prime_list = getPrimes(data.num);
-        var i = Math.floor(Math.random() * prime_list.length);
-        $('#comic_num').remove();
-        $('#comic_view').attr('src', '');   
+        var prime_list = getPrimes(data.num);   // Run getPrimes with input of the current comic number which serves as the largest
+        var i = Math.floor(Math.random() * prime_list.length);  // Randomly generates the index value given the length of the prime_list array
+        $('#comic_num').remove();                               // Removes comic_num div if it exists
+        $('#comic_view').attr('src', '');                       // Sets the src property of the comic_view img tag to empty string
         $.ajax({
-            url: "http://dynamic.xkcd.com/api-0/jsonp/comic/" + prime_list[i],
+            url: "http://dynamic.xkcd.com/api-0/jsonp/comic/" + prime_list[i],  // Url used to make the GET request with the prime_list number appended to the end route
             dataType: 'jsonp',
             cache: false,
-            timeout: 5000,
+            timeout: 5000,                                          // Causes AJAX call to error out if it exceeds five seconds to get a response
             beforeSend: function() {
                 $('#comic_view').hide();
                 $('#viewer').append('<div id="loading_div"><p id="loading">Loading Comic! Please Standby.</p></div>');
@@ -54,9 +53,11 @@
     }
 
 $(document).ready(function() {
-    tester(loadFirstComic);
+
+    tester(loadFirstComic);  // Initial random comic load on document ready
+
     $('#random_Num').click(function(event) {
-        event.preventDefault();
-        tester(loadFirstComic);
+        event.preventDefault();  // Prevents form submission upon click of submit button
+        tester(loadFirstComic);  // Loads another random comic on click of submit button
     });
 });
