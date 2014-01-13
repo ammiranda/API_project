b878586149a12a133cd616ee813a0bccc48c7ee7
Alexander <alexandermichaelmiranda@gmail.com>
2014-01-13 12:05:11 -0800

    Altered code in script.js

--- js/script.js
+++ js/script.js
@@ -14,6 +14,38 @@
  				});
     };
 
+    function appendImages(m) {
+        event.preventDefault();
+        var prime_list = m;
+        var i = Math.floor(Math.random() * prime_list.length);
+        $('#comic_num').remove();
+        $('#comic_view').attr('src', '');   
+        $.ajax({
+            url: "http://dynamic.xkcd.com/api-0/jsonp/comic/" + prime_list[i],
+            dataType: 'jsonp',
+            cache: false,
+            timeout: 5000,
+            beforeSend: function() {
+                $('#comic_view').hide();
+                $('#viewer').append('<div id="loading_div"><p id="loading">Loading Comic! Please Standby.</p></div>');
+                },
+            complete: function() {
+                $('#loading').remove();
+                },
+            success: function(data, textStatus, jqXHR) {
+                console.log(data);
+                $('#loading_div').remove();
+                $('#comic_view').attr('src', data.img);
+                $('#comic_view').show();
+                $('#header').append('<p id="comic_num">Comic Number: <br><span id="prim_num">' + prime_list[i] + '</span></p>');
+                console.log(max(data.num));
+                },
+            error: function(data, textStatus, jqXHR) {
+                $('#comic_view').append('<div id="error_div"><p id="error_msg">Sorry.. something went wrong!  Please click the random button again!"</p></div>');
+                }
+          })
+     };
+
  	
  	function getPrimes(max) { //Function to find a set of prime numbers with the max number of the set as the parameter
         var sieve = [], i, j, primes = [];
@@ -29,40 +61,14 @@
         return primes;
     };
 
+    function add() {
+        return 5;
+    }
+
 $(document).ready(function() {
  	
  	$('#random_Num').click(function(event){
-     	event.preventDefault();
-     	var prime_list = getMax(function(m) {prime_list = m;})();
-     	var i = Math.floor(Math.random() * prime_list.length);
-     	var te;
-     	console.log(i);
-     	$('#comic_num').remove();
-     	$('#comic_view').attr('src', ''); 	
-     	console.log(i);
-     	$.ajax({
-     		url: "http://dynamic.xkcd.com/api-0/jsonp/comic/" + prime_list[i],
-     		dataType: 'jsonp',
-     		cache: false,
-     		timeout: 5000,
-     		beforeSend: function() {
-     			$('#comic_view').hide();
-     			$('#viewer').append('<div id="loading_div"><p id="loading">Loading Comic! Please Standby.</p></div>');
-     			},
-     		complete: function() {
-     			$('#loading').remove();
-     			},
-     		success: function(data, textStatus, jqXHR) {
-     			console.log(data);
-     			$('#loading_div').remove();
-     			$('#comic_view').attr('src', data.img);
-     			$('#comic_view').show();
-     			$('#header').append('<p id="comic_num">Comic Number: <br><span id="prim_num">' + prime_list[i] + '</span></p>');
-     			console.log(max(data.num));
-     	        },
-     		error: function(data, textStatus, jqXHR) {
-     			$('#comic_view').append('<div id="error_div"><p id="error_msg">Sorry.. something went wrong!  Please click the random button again!"</p></div>');
-     				
-     			
-     			}
- 	})})});
+        
+ });
+
+});
